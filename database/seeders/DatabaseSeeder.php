b4159fe08c1dc81694af7770735fa713c71e4ad7
malaksalah8 <malaksalah628@gmail.com>
2026-04-30 16:28:01 +0300

    update project: seeder changes and code cleanup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,21 +5,16 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+User::factory()->create([
+    'name' => 'Instructor One',
+    'email' => 'instructor@demo.com',
+    'password' => bcrypt('password'),
+    'role' => 'instructor',
+]);
 
-class DatabaseSeeder extends Seeder
-{
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
-}
+User::factory()->create([
+    'name' => 'Student One',
+    'email' => 'student@demo.com',
+    'password' => bcrypt('password'),
+    'role' => 'student',
+]);
